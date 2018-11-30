aa2440b46bbda4f87337b170d256f8fa3289e3e2
aretisravani <42770026+aretisravani@users.noreply.github.com>
2018-11-30 08:58:20 +0530

    Update and rename Digita.py to Count.py

---
+++ Count.py
@@ -0,0 +1,6 @@
+z=int(raw_input())
+cout=0
+while(z>0):
+	z=z/10
+	cout=cout+1
+print cout	
--- Digita.py
+++
@@ -1,6 +0,0 @@
-number=int(raw_input())
-count=0
-while(number>0):
-	number=number/10
-	count=count+1
-print count	
